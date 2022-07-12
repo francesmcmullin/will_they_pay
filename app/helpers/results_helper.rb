2bd92da7a008be9af68a78c80b251ff3169c96ca
Frances McMullin <francesmcmullin@protonmail.com>
2022-07-12 15:06:55 +0100

    More helpers for result aggregation

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -10,4 +10,22 @@ module ResultsHelper
 
     @results.where(days_waited: 0).count / @results.count
   end
+
+  def average_coverage
+    return 0 if @results.empty?
+
+    @results.average(:percent_covered)
+  end
+
+  def average_wait_for_reimbursement
+    return 0 if @results.empty?
+
+    @results.average(:days_waited_for_reimbursement)
+  end
+
+  def average_wait_for_treatment
+    return 0 if @results.empty?
+
+    @results.average(:days_waited)
+  end
 end
--- spec/helpers/results_helper_spec.rb
+++ spec/helpers/results_helper_spec.rb
@@ -37,6 +37,44 @@ RSpec.describe ResultsHelper, type: :helper do
       @results = Result.all
       expect(helper.rate_used).to eq(0)
     end
+  end
 
+  describe '#average_wait_for_reimbursement' do
+    it 'returns zero if there are no results' do
+      @results = Result.all
+      expect(helper.average_wait_for_reimbursement).to eq(0)
+    end
+
+    it 'return 7 if one result with 7' do
+      Result.create!(days_waited_for_reimbursement: 7)
+      @results = Result.all
+      expect(helper.average_wait_for_reimbursement).to eq(7)
+    end
+  end
+
+  describe '#average_coverage' do
+    it 'returns zero if there are no results' do
+      @results = Result.all
+      expect(helper.average_coverage).to eq(0)
+    end
+
+    it "return 70% if one result with 70%" do
+      Result.create!(percent_covered: 0.7)
+      @results = Result.all
+      expect(helper.average_coverage).to eq(0.7)
+    end
+  end
+
+  describe '#average_wait_for_treatment' do
+    it 'returns zero if there are no results' do
+      @results = Result.all
+      expect(helper.average_wait_for_treatment).to eq(0)
+    end
+
+    it 'return 7 if one result with 7' do
+      Result.create!(days_waited: 7)
+      @results = Result.all
+      expect(helper.average_wait_for_treatment).to eq(7)
+    end
   end
 end
