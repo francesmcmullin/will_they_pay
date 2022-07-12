2515ba12ccebe50173c3bb259292060873a54cdf
Frances McMullin <francesmcmullin@protonmail.com>
2022-07-12 13:29:07 +0100

    Add devise user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   resources :companies
   resources :treatments
   resources :user_types
-  devise_for :users
   resources :results
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root to: "home#index"
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
