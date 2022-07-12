179b702018d779994e8f0e4f0e5e6c0ea80019ae
Frances McMullin <francesmcmullin@protonmail.com>
2022-07-12 16:17:37 +0100

    Fix DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,11 @@ Provider.create!([
   {name: "St Vincents", description: "St Vincents Hospital", specialty: "N/A", area: "Ireland"},
   {name: "Mater Private", description: "", specialty: "N/A", area: ""}
 ])
+Company.create!([
+  {name: "VHI", description: "Voluntary Health Insurer", area: "Ireland"},
+  {name: "Irish Life", description: "", area: "Ireland"},
+  {name: "BuckEye", description: "has medicaid plans", area: "Ohio"}
+])
 Plan.create!([
   {name: "Basic Medicaid", description: "Bare essentials plan", area: "North Ohio", company_id: 3},
   {name: "PublicPlus care", description: "", area: "", company_id: 1},
@@ -22,13 +27,8 @@ Plan.create!([
   {name: "Gold Plan", description: "Added cover for...", area: "Ohio", company_id: 3},
   {name: "Premier plan", description: "", area: "South Ohio", company_id: 3}
 ])
-Company.create!([
-  {name: "VHI", description: "Voluntary Health Insurer", area: "Ireland"},
-  {name: "Irish Life", description: "", area: "Ireland"},
-  {name: "BuckEye", description: "has medicaid plans", area: "Ohio"}
-])
 User.create!([
-  {email: "blah@example.org", encrypted_password: "$2a$12$rTlM9BOHAqEKlN0A4c11IOLcJTSd/MsQFqETYJTcJBgMnOkSA8s3G", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_type_id: 1},
-  {email: "person@example.org", encrypted_password: "$2a$12$FNAmXzHnghi5TklZZt1I/eiliWQwRPil66DLPGpOk0TDiP963vJXC", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_type_id: 1},
-  {email: "alice@example.org", encrypted_password: "$2a$12$DwgItoZ5gyMf835KqFwGPuvgcToTIdJ4wyL4GHAxHtc5/0iI7nOxu", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_type_id: 1}
+  {email: "blah@example.org", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_type_id: 1},
+  {email: "person@example.org", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_type_id: 1},
+  {email: "alice@example.org", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_type_id: 1}
 ])
